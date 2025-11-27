4d610dd8f889713975a938c46ed5993368da049a
forkasiewicz <111523660+forkasiewicz@users.noreply.github.com>
2025-11-27 21:00:06 +0100

    add thrust

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 local Vec = require("vector")
 local Line = require("line")
 
-SIZE = 0.75
-SPEED = 5
+SIZE = 0.6
+SPEED = 50
 -- rotations per sec
 ROT_SPEED = 1
 
@@ -15,38 +15,59 @@ local ship = {
 }
 
 local ship_rotation = 0
-local thrustVec = Vec.new(SPEED, 0)
 local offset = Vec.new(0, 0)
+local velocity = Vec.new(0, 0)
+local delta_time = 0
 
 function love.load()
-    WindowCenter = Vec.new(love.graphics.getDimensions()):scale(0.5)
-    offset = WindowCenter
+    offset = Vec.new(love.graphics.getDimensions()):scale(0.5)
 end
 
 function love.update(dt)
-    -- have two vectors:
-    --      max speed vector
-    --      current speed vector
-    -- upon pressing w we will add the max speed vector to the current speed vector
-    -- every frame current speed vector will be constantly going down
+    delta_time = dt
+    -- lerp function
+    -- offset = offset:add(destination:sub(offset):scale(dt))
+
+    offset = offset:add(velocity:scale(dt * 5))
 
     if love.keyboard.isDown("up") then
-        offset = offset:add(thrustVec)
+        local thrust = Vec.new(SPEED, 0):rot(ship_rotation)
+        velocity = velocity:add(thrust:scale(dt))
     end
 
     if love.keyboard.isDown("left") then
-        thrustVec = thrustVec:rot(-ROT_SPEED * math.pi * 2 * dt)
         ship_rotation = ship_rotation - ROT_SPEED * math.pi * 2 * dt
     end
 
     if love.keyboard.isDown("right") then
-        thrustVec = thrustVec:rot(ROT_SPEED * math.pi * 2 * dt)
         ship_rotation = ship_rotation + ROT_SPEED * math.pi * 2 * dt
+    end
+
+    if offset.x >= love.graphics.getWidth() then
+        offset.x = 1
+    end
+
+    if offset.y >= love.graphics.getHeight() then
+        offset.y = 1
+    end
+
+    if offset.x <= 0 then
+        offset.x = love.graphics.getWidth()
+    end
+
+    if offset.y <= 0 then
+        offset.y = love.graphics.getHeight()
     end
 end
 
 function love.draw()
-    love.graphics.circle("fill", offset.x, offset.y, 1)
+    love.graphics.print(string.format("FPS: %i\ndelta_time: %f", 1 / delta_time, delta_time))
+
+    -- debug dot to check the center of the ship
+    -- love.graphics.circle("fill", offset.x, offset.y, 1)
+
+    -- debug line to check the current direction
+    -- Line.new(offset, Vec.new(100, 0):rot(ship_rotation):add(offset)):draw()
 
     for _, line in ipairs(ship) do
         line = line:scale(SIZE)
--- vector.lua
+++ vector.lua
@@ -9,6 +9,18 @@ function Vec:add(vector)
     return Vec.new(self.x + vector.x, self.y + vector.y)
 end
 
+function Vec:sub(vector)
+    return Vec.new(self.x - vector.x, self.y - vector.y)
+end
+
+function Vec:normalized()
+    local root = math.sqrt(self.x ^ 2 + self.y ^ 2)
+    return Vec.new(
+        self.x / root,
+        self.y / root
+    )
+end
+
 function Vec:rot(n)
     return Vec.new(
         self.x * math.cos(n) - self.y * math.sin(n),
